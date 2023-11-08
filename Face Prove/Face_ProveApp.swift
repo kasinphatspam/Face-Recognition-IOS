555f91d0fe4bc862d7f12fb49e66e523f14d4e51
kasinphatspam <kasinphat@icloud.com>
2023-11-08 16:08:13 +0700

    Initial Commit

---
+++ Face Prove/Face_ProveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_ProveApp.swift
+//  Face Prove
+//
+//  Created by Kasinphat Ketchom on 8/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Face_ProveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Face ProveUITests/Face_ProveUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Face_ProveUITestsLaunchTests.swift
+//  Face ProveUITests
+//
+//  Created by Kasinphat Ketchom on 8/11/2566 BE.
+//
+
+import XCTest
+
+final class Face_ProveUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
